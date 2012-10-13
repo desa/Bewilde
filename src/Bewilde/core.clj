484c637dded4cb4326bdf42c8847e5ca1993c6d7
Michael Desa <mjdesa@gmail.com>
2012-10-13 02:01:49 -0700

    added all basic shapes and objects

--- src/Bewilde/core.clj
+++ src/Bewilde/core.clj
@@ -7,32 +7,68 @@
 
 (defn make-object [obj]
   {:tag obj
-   :id nil
-   :attrs nil
-   :content nil})
+   :attrs {}
+   :content []})
 
 (defmacro defobject [obj & body]
-  `(let [o# (-> (make-object ~(keyword obj))
-                 (assoc ~@body :id (gensym "obj")))]
+  `(let [o# (make-object ~(keyword obj))]
      (def ~obj o#)))
 
 (defobject point)
 
 (defn new-point [[x y]]
-  (assoc point :attrs {:x x :y y}))
+  (assoc point :attrs {:id (gensym "point") :x x :y y}))
 
 (defobject line)
 
-(defn new-line [p1 p2]
-  (assoc line :content [{:point1 p1} {:point2 p2}]))
+(defn new-line [p1 p2]   
+  (let [ps (map new-point [p1 p2])]
+    (assoc line :attrs {:id (gensym "line")} :content (vec ps))))
+
+(defobject radius)
+
+(defn new-radius [r]
+  (assoc radius :content [r]))
+
+(defobject theta)
+
+(defn new-theta [t]
+  (assoc theta :content [t]))
 
 (defobject circle)
 
-(defn new-circle [p r]
-  (assoc circle :attrs {:radius r} :content [{:point p}]))
+(defn new-circle [p r t1 t2]
+  (let [point (new-point p)
+        rad (new-radius r)
+        theta1 (new-theta t1)
+        theta2 (new-theta t2)]
+    (assoc circle
+      :attrs {:id (gensym "circle")}
+      :content [point rad theta1 theta2])))
 
 (defobject polyline)
 
-(defn new-polyline [x] x)
+(defn new-polyline [p1 p2 & p3s]
+  (let [p2s (conj p3s p2)
+        p1s (conj p2s p1)
+        ps  (map new-point p1s)]
+    (assoc polyline
+      :attrs {:id (gensym "polyline")}
+      :content (vec ps))))
 
 (defobject bezier)
+
+(defn new-bezier [p1 p2 p3 p4]
+  (let [ps (map new-point [p1 p2 p3 p4])]
+    (assoc bezier
+      :attrs {:id (gensym "bezier")}
+      :content (vec ps))))
+
+(defobject group)
+
+(defn group-obj [o1 o2 & o3]
+  (let [o2s (conj o3 o2)
+        os (conj o2s o1)]
+    (assoc group
+      :attrs {:id (gensym "group")}
+      :content (vec os))))
